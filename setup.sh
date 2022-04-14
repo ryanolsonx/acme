2e81dc7e8506c68786b01f41db8d0dae941d55a9
Ryan Olson <rolson@swipeclock.com>
2022-04-14 15:38:00 -0600

    Start kind of fresh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 # Use acme for editor
 export EDITOR=E
+alias acme="acme -a -f /mnt/font/Menlo-Regular/10a/font"
 
 export PLAN9="$HOME/apps/plan9port"
 export PATH="$PATH":"$PLAN9/bin":"$HOME/acme/bin"
+
+plumber
+cat "$PLAN9/plumb/basic" | 9p write plumb/rules
+
